3a7b0be0cc9cc41d1894916696bb3fad75c3ae05
Justin Elliott <Justinelliott11375@gmail.com>
2018-09-09 17:41:12 -0500

    complete checkpoint 6 work

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -6,8 +6,8 @@ const topicController = require("../controllers/topicController");
 router.get("/topics", topicController.index);
 router.get("/topics/new", topicController.new);
 router.post("/topics/create", topicController.create);
-router.get("/topics/:id", topicController.show);
 router.post("/topics/:id/destroy", topicController.destroy);
 router.get("/topics/:id/edit", topicController.edit);
+router.get("/topics/:id", topicController.show);
 
 module.exports = router;
