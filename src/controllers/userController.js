828bdd254ebcf4d651c2bdcb4b9bb3beb3e1d840
Justin Elliott <Justinelliott11375@gmail.com>
2018-09-16 23:45:53 -0500

    authorization checkpoint work complete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,7 +16,7 @@ module.exports = {
         userQueries.createUser(newUser, (err, user) => {
             if(err) {
                 req.flash("error", err);
-                req.redirect("/users/sign_up");
+                res.redirect("/users/sign_up");
             } else {
                 passport.authenticate("local")(req, res, () => {
                     req.flash("notice", "You've successfully signed in!");
